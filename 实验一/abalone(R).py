7d0d171a54027f4e898d796ec0cd7a04e3ae2157
duuook <3022531349@qq.com>
2024-04-10 18:09:19 +0800

    rename

--- 实验一/abalone(R).py
+++ 实验一/abalone(R).py
@@ -6,9 +6,3 @@ abalone = fetch_ucirepo(id=1)
 # data (as pandas dataframes) 
 X = abalone.data.features 
 y = abalone.data.targets 
-  
-# metadata 
-print(abalone.metadata) 
-  
-# variable information 
-print(abalone.variables) 
